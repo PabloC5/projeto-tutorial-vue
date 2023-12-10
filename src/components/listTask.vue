f0be790e8a52ceafd9b77dc25c5e084f10f66090
pablo correia <pablocorreia2012@gmail.com>
2023-12-10 17:02:04 -0300

    adicionado editar e finalizado tarefa

--- src/components/listTask.vue
+++ src/components/listTask.vue
@@ -19,12 +19,14 @@
                             id="taskInput"
                             v-model="newTask"
                             required
-                            placeholder="Enter a new task"
+                            :placeholder="editTaskOrNo ? 'Editando tarefa' : 'Enter a new task'"
                         ></b-input>
                         </b-form-group>
                     </b-col>
                     <b-col>
-                        <b-button class="ajusta" type="submit" variant="primary">Adicionar tarefa</b-button>
+                        <b-button class="ajusta" type="submit" variant="primary">
+                            {{ editTaskOrNo ? 'Salvar' : 'Adicionar Tarefa' }}
+                        </b-button>
                     </b-col>
                 </b-row>
                 
@@ -46,25 +48,40 @@
         ],
         newTask: "",
         fields: ["Nome", "actions"],
+        editTaskOrNo: false,
+        idEdit: null
       };
     },
     methods: {
       addTask() {
-        if (this.newTask.trim() !== "") {
-          this.tasks.push({ id: Date.now(), text: this.newTask });
+        if (this.newTask.trim() !== "" && !this.editTaskOrNo) {
+          this.tasks.push({ id: Date.now(), Nome: this.newTask });
           this.newTask = "";
+        } else if (this.editTaskOrNo) {
+            this.tasks[this.idEdit].Nome = this.newTask;
+            this.editTaskOrNo = false;
+            this.newTask = "";
         }
       },
       removeTask(taskId) {
         this.tasks = this.tasks.filter((task) => task.id !== taskId);
       },
+
+      editTask(editId) {
+        this.editTaskOrNo = true
+        let indiceEdit = this.tasks.findIndex(task => task.id === editId);
+        this.newTask = this.tasks[indiceEdit].Nome
+        this.idEdit = indiceEdit;
+        // this.tasks[indiceEdit].text = this.newTask;
+        console.log(this.tasks[indiceEdit].Nome);
+      }
     },
   };
   </script>
 
 <style>
     .ajusta {
-        margin-left: -442px;
+        margin-left: -430px;
     }
 </style>
   
